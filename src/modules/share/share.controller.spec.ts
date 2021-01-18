5ed7c9570e58121c5db8bbda20005618dcde5b04
Dmitry Dergunov <dmitry@dergunov.net>
2021-01-18 19:24:43 +0300

    fix tests

--- src/modules/share/share.controller.spec.ts
+++ src/modules/share/share.controller.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ShareController } from './share.controller';
+import { ShareService } from './share.service';
 
 describe('ShareController', () => {
   let controller: ShareController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [ShareService],
       controllers: [ShareController],
     }).compile();
 
--- src/modules/share/share.service.spec.ts
+++ src/modules/share/share.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ShareService } from './share.service';
+import { ShareController } from './share.controller';
 
 describe('ShareService', () => {
   let service: ShareService;
@@ -7,6 +8,7 @@ describe('ShareService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [ShareService],
+      controllers: [ShareController],
     }).compile();
 
     service = module.get<ShareService>(ShareService);
